1b393c8646c0999472a24b4d716f55ba139848da
Andreas García <acubera@proton.me>
2024-02-27 10:42:07 +0100

    init firing_range

---
+++ firingRange/src/__main__.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+from re import match
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi():
+
+    name = input("What's your name? ")
+    print(f"Hi, {name}")
+    input()
+
+
+def main():
+    print_hi()
+
+
+if __name__ == "__main__":
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
